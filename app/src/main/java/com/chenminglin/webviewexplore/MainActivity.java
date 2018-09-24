09737a1258c5de5183fa0c6da14ce714b0b04907
chenminglin <806663326@qq.com>
2018-09-24 21:37:31 +0800

    给WebView加一些设置。

--- app/src/main/java/com/chenminglin/webviewexplore/MainActivity.java
+++ app/src/main/java/com/chenminglin/webviewexplore/MainActivity.java
@@ -1,9 +1,11 @@
 package com.chenminglin.webviewexplore;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -19,6 +21,39 @@ public class MainActivity extends AppCompatActivity {
         WebView webView = findViewById(R.id.webview);
 
         webView.loadUrl(url);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+
+        //5.0以上开启混合模式加载
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        //允许js代码
+        webSettings.setJavaScriptEnabled(true);
+        //允许SessionStorage/LocalStorage存储
+        webSettings.setDomStorageEnabled(true);
+        //禁用放缩
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setBuiltInZoomControls(false);
+        //禁用文字缩放
+        webSettings.setTextZoom(100);
+        //10M缓存，api 18后，系统自动管理。
+        webSettings.setAppCacheMaxSize(10 * 1024 * 1024);
+        //允许缓存，设置缓存位置
+        webSettings.setAppCacheEnabled(true);
+
+        webSettings.setAppCachePath(this.getDir("appcache", 0).getPath());
+        //允许WebView使用File协议
+        webSettings.setAllowFileAccess(true);
+        //不保存密码
+        webSettings.setSavePassword(false);
+        //设置UA
+        webSettings.setUserAgentString(webSettings.getUserAgentString() +"/test");
+        //自动加载图片
+        webSettings.setLoadsImagesAutomatically(true);
 
         webView.setWebViewClient(new WebViewClient(){
             @Override
---
+++ app/src/main/java/com/chenminglin/webviewexplore/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.chenminglin.webviewexplore;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
